1b0650376676226795fd53cc63c46057ec9ad22a
kinganik6263 <161469632+kinganik6263@users.noreply.github.com>
2024-05-30 15:09:57 +0800

    Update 0bot.js

--- modules/commands/0bot.js
+++ modules/commands/0bot.js
@@ -6,7 +6,7 @@ module.exports.config = {
   name: "bot",
   version: "0.0.2",
   hasPermssion: 0,
-  usePrefix: true,
+  usePrefix: false,
   credits: "Nayan",
   description: "talk with bot",
   commandCategory: "user",
